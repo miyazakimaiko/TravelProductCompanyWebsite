edd6585b6787ab33282ad134bca53d7a038354fc
miyazakimaiko <myzkmik19922@gmail.com>
2021-05-14 14:52:16 +0100

    Delete comments

--- library/js/loading-animation.js
+++ library/js/loading-animation.js
@@ -1,17 +1,7 @@
 $(window).on('load',function(){
-    $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+    $("#splash-logo").delay(1200).fadeOut('slow');
   
-    //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-    $("#splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
-    
-        $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
-  
+    $("#splash").delay(1500).fadeOut('slow',function(){
+        $('body').addClass('appear');
     });
-    //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
-    
-   //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
-    $('.splashbg').on('animationend', function() {    
-        //この中に動かしたいJSを記載
-    });
-    //=====ここまで背景が伸びた後に動かしたいJSをまとめる
 });
