a72b74dc53ab8c142c67c863902929cdd60b8296
miyazakimaiko <myzkmik19922@gmail.com>
2021-05-14 14:52:57 +0100

    Remove video when smaller devices

--- library/js/slideshow.js
+++ library/js/slideshow.js
@@ -1,10 +1,20 @@
+// Remove video from the slide on small devices
+$(".slide-4").each(function(){
+  var width = $(window).width();
+  var video = $(this);
+
+  if (width < 768) {
+    video.remove();
+  }
+});
+
 $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() {
   $('#slideshow > div:first')
-    .fadeOut(2000)
+    .fadeOut(3000)
     .next()
-    .fadeIn(2000)
+    .fadeIn(3000)
     .end()
     .appendTo('#slideshow');
 }, 6000);
