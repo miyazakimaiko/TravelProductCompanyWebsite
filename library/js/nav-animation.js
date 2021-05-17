866d4a7feff8f76ea53e76abb122f6fee455c513
miyazakimaiko <myzkmik19922@gmail.com>
2021-05-17 14:28:08 +0100

    Add variable for brown color

--- library/js/nav-animation.js
+++ library/js/nav-animation.js
@@ -1,66 +1,51 @@
 let NavIsOpen = false;
 
-function SlideIn(n) {
+function OpenNavigation(n) {
   let i;
   if (NavIsOpen == false) {
-    showBackground()
-    cursor.classList.add("whiten");
-    for (i = n; i < 6; i++) {
-      SlideInMenuLink(i)
-    }
     NavIsOpen = true
-  } else if (NavIsOpen == true) {
-    for (i = n; i < 6; i++) {
-      SlideOutMenuLink(i)
-    }
-    hideBackground()
-    cursor.classList.remove("whiten");
-    NavIsOpen = false
-  }
-}
 
-function showBackground(){
-  const navScreen = document.getElementById("nav-screen");
-  navScreen.style.display = "block";
+    // Show background
+    let navScreen = document.getElementById("nav-screen");
+    navScreen.style.display = "block";
+
+    // Change custom cursor's colour
+    cursor.classList.add("whiten");
+
+    // Slide in menu links
+    setTimeout(SlideInMenuLink, 200)
+
+  } else if (NavIsOpen == true) {
+    NavIsOpen = false
+
+    // Hide menu links first
+    SlideOutMenuLink()
+
+    // Then hide background
+    setTimeout(hideBackground, 800)
+
+    // Change the colour of custom cursor
+    cursor.classList.remove("whiten");
+  }
 }
 
 function hideBackground(){
-  setTimeout(displayNone, 1000)
-
-  function displayNone() {
-    const navScreen = document.getElementById("nav-screen");
-    navScreen.style.display = "none";
-  }
+  let navScreen = document.getElementById("nav-screen");
+  navScreen.style.display = "none";
 }
 
-function SlideInMenuLink(n) {
-  let elem = document.getElementsByClassName("menu-link")[n];
-  let pos = -1 * window.innerWidth - n * 100;
-  let id = setInterval(frame, 2);
+function SlideInMenuLink() {
+  let elems = document.querySelectorAll(".menu-link-text");
 
-  function frame() {
-    if (pos >= -10) {
-      clearInterval(id);
-      elem.style.right = 0 + 'px';
-    } else {
-      pos += 15;
-      elem.style.right = pos + 'px';
-    }
-  }
+  elems.forEach(el => {
+    el.classList.add("is-animated");
+  })
 }
 
-function SlideOutMenuLink(n) {
-  let elems = document.getElementsByClassName("menu-link")[n];
-  let poss = 0;
-  let ids = setInterval(frames, 2);
+function SlideOutMenuLink() {
+  let elems = document.querySelectorAll(".menu-link-text");
 
-  function frames() {
-    if (poss <= -1 * window.innerWidth) {
-      clearInterval(ids);
-      elems.style.right = -1 * window.innerWidth + 'px';
-    } else {
-      poss += -7 - n * 2;
-      elems.style.right = poss + 'px';
-    }
-  }
+  elems.forEach(el => {
+    el.classList.remove("is-animated");
+  })
 }
